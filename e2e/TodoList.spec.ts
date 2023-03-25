ed6d7613fd051cb1ca61a7b1952e865fa2223919
Johan Guse <johanguse@gmail.com>
2023-03-25 15:37:37 -0300

    Add e2e test with @playwright

---
+++ e2e/TodoList.spec.ts
@@ -0,0 +1,10 @@
+import { test } from '@playwright/test'
+
+test('visits the app root url and check if new todo has print on screen', async ({ page }) => {
+  await page.goto('/')
+  await page.getByLabel('todo').fill('New To-Do from Playwright')
+  await page.getByRole('button', { name: 'Add To-Do' }).click()
+  await page.waitForSelector('text=New To-Do from Playwright')
+  await page.waitForSelector('text=Toggle All Complete')
+  await page.waitForSelector("text=Remove All To-do's")
+})
