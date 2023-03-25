dc5098116f0a09d14b51837cba2a199571066b0c
Johan Guse <johanguse@gmail.com>
2023-03-25 11:19:15 -0300

    Update list and test

--- src/components/__tests__/TodoList.spec.ts
+++ src/components/__tests__/TodoList.spec.ts
@@ -1,6 +1,6 @@
 import { setActivePinia, createPinia } from 'pinia'
 import { useTodoListStore } from '@/stores/todoList'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import TodoList from '../TodoList.vue'
 
@@ -13,5 +13,13 @@ describe('TodoList', () => {
     const wrapper = mount(TodoList)
     expect(useTodoListStore().todoList).toHaveLength(0)
     expect(wrapper.find('.no-todos-found').text()).toBe('No To-Do found')
+  })
+  it('renders a list of todo items', async () => {
+    useTodoListStore().addTodoItem('Todo Item 1')
+    useTodoListStore().addTodoItem('Todo Item 2')
+    useTodoListStore().addTodoItem('Todo Item 3')
+    expect(useTodoListStore().todoList).toHaveLength(3)
+    const wrapper = mount(TodoList)
+    expect(wrapper.findAll('.todo-list__item').length).toBe(3)
   })
 })
